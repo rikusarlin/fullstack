0f98e6e535ed33e2ceaf62fd93b20c0cfb17d7e7
Riku Sarlin <riku.sarlin@iki.fi>
2019-12-18 18:33:58 +0200

    task 4.10

--- osa4/blogilista/tests/blog_api.test.js
+++ osa4/blogilista/tests/blog_api.test.js
@@ -30,38 +30,57 @@ beforeEach(async () => {
   await blogObject.save()
 })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+describe('fetch all blogs', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body.length).toBe(initialBlogs.length)
+  })
+  test('a specific blog is within the returned blogs', async () => {
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map(r => r.title)
+    expect(titles).toContain('Kela, Jumalasta seuraava')
+  })
+  test('id field is returned without preceding underscore', async () => {
+    const response = await api.get('/api/blogs')
+    const blog = response.body[0]
+    expect(blog.id).toBeDefined()
+  })
 })
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
-
-  expect(response.body.length).toBe(initialBlogs.length)
+describe('insert new blog', () => {
+  test('number of blogs increases when a new blog is added', async () => {
+    let response = await api.get('/api/blogs')
+    const blogsBefore = response.body.length
+    const blog = new Blog({
+      title:'Use cases considered harmful',
+      author:'A.J.H Simons',
+      url:'https://ieeexplore.ieee.org/document/779012',
+      likes:30
+    })
+    await blog.save()
+    response = await api.get('/api/blogs')
+    const blogsAfter = response.body.length
+    expect(blogsAfter).toBe(blogsBefore+1)
+  })
+  test('an inserted blog can be found after addition', async () => {
+    const blog = new Blog({
+      title:'Use cases considered harmful',
+      author:'A.J.H Simons',
+      url:'https://ieeexplore.ieee.org/document/779012',
+      likes:30
+    })
+    await blog.save()
+    const response = await api.get('/api/blogs')
+    const titles = response.body.map(r => r.title)
+    expect(titles).toContain('Use cases considered harmful')
+  })
 })
-
-test('a specific blog is within the returned blogs', async () => {
-  const response = await api.get('/api/blogs')
-
-  const titles = response.body.map(r => r.title)
-
-  expect(titles).toContain(
-    'Kela, Jumalasta seuraava'
-  )
-})
-
-test('id field is returned without preceding underscore', async () => {
-  const response = await api.get('/api/blogs')
-
-  const blog = response.body[0]
-  console.log(blog)
-
-  expect(blog.id).toBeDefined()
-})
-
 
 afterAll(() => {
   mongoose.connection.close()
