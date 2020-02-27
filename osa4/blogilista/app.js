580cd8d395f6cc539a685b056c1fada2933611cf
Riku Sarlin <riku.sarlin@iki.fi>
2020-02-27 21:33:58 +0200

    part7-19

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -29,6 +29,12 @@ app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/login', loginRouter)
 
+// Add testing router (do reset database) in test env
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
