11f7e2b187ee936a77ac8639d05ebfee53f17451
Riku Sarlin <riku.sarlin@gmail.com>
2021-09-15 21:45:37 +0300

    Exercise 10.3

---
+++ osa10/rate-repository-app/src/components/RepositoryItem.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const styles = StyleSheet.create({
+  separator: {
+    height: 10,
+  },
+});
+
+const RepositoryItem = ({ fullName, description, language, forksCount, stargazersCount,
+    ratingAverage, reviewCount})  => {
+  return (
+    <View style={styles.item}>
+      <Text>Full Name: {fullName}</Text>
+      <Text>Description: {description}</Text>
+      <Text>Language: {language}</Text>
+      <Text>Stars: {stargazersCount}</Text>
+      <Text>Forks: {forksCount}</Text>
+      <Text>Reviews: {reviewCount}</Text>
+      <Text>Rating: {ratingAverage}</Text>
+    </View>
+  );
+};
+
+export default RepositoryItem;
